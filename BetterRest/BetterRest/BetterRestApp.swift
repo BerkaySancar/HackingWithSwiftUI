e8f9dcaadc8b61dc88fe5c558377ea56ced02ffd
Berkay Sancar <sancarberkay@gmail.com>
2022-11-28 22:41:38 +0300

    add BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Berkay Sancar on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
